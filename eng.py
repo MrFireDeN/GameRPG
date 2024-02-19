68a521725b29a08eac97e50ca2aafca11eb05de2
MrFireDeN <36342234+MrFireDeN@users.noreply.github.com>
2024-02-19 20:35:30 +0300

    Initial commit

---
+++ eng.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+import os
+
+from flask import Flask, g, render_template, request, jsonify, url_for, send_file, redirect
+
+import settings
+
+app = Flask(__name__, template_folder="templates")
+
+app.config['SECRET_KEY'] = settings.SECRET_KEY
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.htm'), 404
+
+
+@app.route("/<page_name>/")
+def main(page_name):
+    return render_template(page_name+'.htm')
+
+@app.route("/")
+def index():
+    return redirect(url_for('main', page_name='main'))
+
+
+if __name__ == "__main__":
+##    #Еще один способ добавления статической дирректории
+    from werkzeug.middleware.shared_data import SharedDataMiddleware
+    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+      '/': os.path.join(os.path.dirname(__file__), 'static')
+    })
+    app.run(host='0.0.0.0', port=5000, debug=True)
