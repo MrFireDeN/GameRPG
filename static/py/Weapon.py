342a3842701da1d327bb164d8b2732357d16695e
MrFireDeN <36342234+MrFireDeN@users.noreply.github.com>
2024-02-22 17:09:40 +0300

    feat: добавление геттор урона и уровня оружия

--- static/py/Weapon.py
+++ static/py/Weapon.py
@@ -7,6 +7,14 @@ class Weapon(Item):
 
         self._damage = damage
 
+    @property
+    def damage(self):
+        return self._damage
+
+    @property
+    def level(self):
+        return self._level
+
     # Информация о оружии
     def getInfo(self):
         return super().getInfo(), self._damage
