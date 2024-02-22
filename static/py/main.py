cb389ea23c4132659a2adf76af6c34ec6dab81eb
MrFireDeN <36342234+MrFireDeN@users.noreply.github.com>
2024-02-22 17:57:11 +0300

    Update main.py

--- static/py/main.py
+++ static/py/main.py
@@ -3,13 +3,16 @@ from Weapon import Weapon
 from Item import Item
 from Damage import Damage
 from Player import Player
+from Armor import Armor
+
 
 def main():
     Petya = Player("Petya", 1)
 
-    sword = Weapon("меч", 1, 100, Damage(fire = 10))
+    sword = Weapon("меч", 1, 100, fire = 10)
+    armor = Armor("Armor", 5, 1000, slash= 5, fire = 5)
 
-    print(Damage.calculate_damage(sword.damage), sword.level, Petya.getArmor(), Petya.getLevel())
+    print(Damage.calculate_damage(sword.damage, sword.level, armor.defence, armor.level))
 
 if __name__ == '__main__':
     main()
