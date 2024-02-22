a997955bf03e6b1eaa26e77ce457bbbef7ad71e4
MrFireDeN <36342234+MrFireDeN@users.noreply.github.com>
2024-02-22 16:46:26 +0300

    Update main.py

--- static/py/main.py
+++ static/py/main.py
@@ -1,17 +1,22 @@
-import Actor, Player
+from Actor import Actor
+from Weapon import Weapon
+from Item import Item
+from Damage import Damage
 
 def main():
-    #Player = player.Player("Илюха")
-    #Player.getInfo()
 
-    Petya = Actor.Actor("Petya", 1)
+    Petya = Actor("Petya", 1)
 
-    print(Petya.calculateDamage(Petya))
+    print(Petya.calculateDamage(Petya.getLevel()))
 
     Petya.giveExperience(1000)
     if not (Petya.takeDamage(100)):
         del(Petya)
 
+    item1 = Item("Weapon", 2, 10)
+
+    sword = Weapon("меч", 1, 100, Damage(fire = 10))
+    print(sword.getInfo())
 
 if __name__ == '__main__':
     main()
