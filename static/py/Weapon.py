3986bc91aca9187bc58511a013aaa54fcf6689de
MrFireDeN <36342234+MrFireDeN@users.noreply.github.com>
2024-02-22 17:56:39 +0300

    refactor: изменения в классе Weapon

--- static/py/Weapon.py
+++ static/py/Weapon.py
@@ -1,11 +1,16 @@
 from Item import Item
 from Damage import Damage
+from Transform import Transform
 
-class Weapon(Item):
-    def __init__(self, name: str, level: int, value: int, damage: Damage):
-        super().__init__(name, level, value)
+class Weapon(Item, Damage):
+    def __init__(self, name: str, level: int = 0, value: int = 0,
+                 transform: Transform = Transform(0, 0),
+                 slash: int = 0, pierce: int = 0, blunt: int = 0,
+                 fire: int = 0, ice: int = 0, poison: int = 0, electric: int = 0) -> None:
+        Item.__init__(self, name, level, value, transform)
+        Damage.__init__(self, slash, pierce, blunt, fire, ice, poison, electric)
 
-        self._damage = damage
+        self._damage = Damage(slash, pierce, blunt, fire, ice, poison, electric)
 
     @property
     def damage(self):
