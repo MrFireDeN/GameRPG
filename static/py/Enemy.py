ed2c57f71eb006c541237b6284667b474b146c9b
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-02-26 10:19:23 +0300

    feat: создание класса Enemy

--- static/py/Enemy.py
+++ static/py/Enemy.py
@@ -1,6 +1,6 @@
 from Actor import Actor
 from Transform import Transform
 
-class Player(Actor):
+class Enemy(Actor):
     def __init__(self, name: str = '', level: int = 0, isAlive: bool = True, transform: Transform = Transform(0, 0)):
         super().__init__(name, level, isAlive, transform)
