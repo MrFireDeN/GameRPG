9f325813b0b4892c29e7c56c83e997a0ade464a7
MrFireDeN <36342234+MrFireDeN@users.noreply.github.com>
2024-04-08 21:41:26 +0300

    fix: незначительные улучшения

--- static/js/inventory.js
+++ static/js/inventory.js
@@ -24,15 +24,15 @@ $(document).ready(function() {
         for (var i = 0; i < items.length; i++) {
             var item = items[i];
             var itemId = 'item_' + i;
-            var $itemImg = $('#' + itemId); // Select the image element
+            var $itemImg = $('#' + itemId); // Выбираем элемент изображения
 
             if ($itemImg.length > 0) {
-                // If the image element exists
-                var itemSrc = '../static/img/items/' + item.name + '.png'; // Assuming item.name holds the image filename
-                $itemImg.attr('src', itemSrc); // Update the image source
-                $itemImg.attr('alt', item.name); // Update the alt attribute if needed
+                // Если элемент изображения существует
+                var itemSrc = '../static/img/items/' + item.name + '.png'; // Предполагаем, что item.name содержит имя файла изображения
+                $itemImg.attr('src', itemSrc); // Обновляем источник изображения
+                $itemImg.attr('alt', item.name); // Обновляем атрибут alt при необходимости
             } else {
-                console.error('Image element with id ' + itemId + ' not found.');
+                console.error('Элемент изображения с id ' + itemId + ' не найден.');
             }
 
             var weapon = data.weapon;
@@ -41,45 +41,29 @@ $(document).ready(function() {
             var consumable2 = data.consumable2;
             var consumable3 = data.consumable3;
 
-            // Update weapon
-            if (weapon) {
-                $('#weapon img').attr('src', '../static/img/items/' + weapon.name + '.png');
-                $('#weapon p').text(weapon.name);
-            }
-
-            // Update armor
-            if (armor) {
-                $('#armor img').attr('src', '../static/img/items/' + armor.name + '.png');
-                $('#armor p').text(armor.name);
-            }
-
-            // Update consumable1
-            if (consumable1) {
-                $('#consumable1 img').attr('src', '../static/img/items/' + consumable1.name + '.png');
-                $('#consumable1 p').text(consumable1.name);
-            }
-
-            // Update consumable2
-            if (consumable2) {
-                $('#consumable2 img').attr('src', '../static/img/items/' + consumable2.name + '.png');
-                $('#consumable2 p').text(consumable2.name);
-            }
-
-            // Update consumable3
-            if (consumable3) {
-                $('#consumable3 img').attr('src', '../static/img/items/' + consumable3.name + '.png');
-                $('#consumable3 p').text(consumable3.name);
-            }
+        
         }
+
+        updateElement('#weapon', weapon.name);
+        updateElement('#armor', armor.name);
+        updateElement('#consumable1', consumable1.name);
+        updateElement('#consumable2', consumable2.name);
+        updateElement('#consumable3', consumable3.name);
     }
 
+    function updateElement(selector, name) {
+        $(selector + ' img').attr('src', `../static/img/items/${name}.png`);
+        $(selector + ' p').text(name);
+    }
+
+
     $(document).on('click', '.item-link', function(e) {
-        e.preventDefault(); // Prevent the default action
-        var itemIndex = $(this).data('index'); // Assuming data-index attribute contains the item index
+        e.preventDefault(); // Предотвращаем выполнение стандартного действия
+        var itemIndex = $(this).data('index'); // Предполагаем, что атрибут data-index содержит индекс элемента
         $.ajax({
             type: 'POST',
             url: '/get-item/',
-            data: {'item': itemIndex}, // Send item index to the server
+            data: {'item': itemIndex}, // Отправляем индекс элемента на сервер
             success: function(response) {
                 console.log(response);
                 if (response.name) {
@@ -87,17 +71,16 @@ $(document).ready(function() {
                 }
             },
             error: function(xhr, status, error) {
-                console.error('Error while retrieving item:', error);
+                console.error('Ошибка при получении элемента:', error);
             }
         });
     });
 
     function updateItem(data) {
-        name = data.name;
-        note = data.note;
-
-        $('.info img').attr('src', '../static/img/items/' + name + '.png');
-        $('.info h2').text(name);
-        $('.info p').text(note);
+        var itemName = data.name;
+        var itemNote = data.note;
+        $('.info img').attr('src', '../static/img/items/' + itemName + '.png');
+        $('.info h2').text(itemName);
+        $('.info p').text(itemNote);
     }
 });
